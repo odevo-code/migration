58fcdc60c31d71f8abc69bd20277de960657a96f
stekman08 <fredrik.ekman@odevo.com>
2024-06-13 16:05:45 +0200

    Some improvements in remove_repos

--- remove_repos.sh
+++ remove_repos.sh
@@ -10,17 +10,53 @@ KEEP_REPOS="skills kw.cypresstests-gustav"
 IFS=' ' read -r -a REPO_ARRAY <<<"$KEEP_REPOS"
 
 # Get a list of all repositories in the organization
-IFS=$'\n' repos=("$(gh repo list -L 100 $ORG_NAME | awk '{print $1}')")
+repos=()
+while IFS= read -r line; do
+    repos+=("$line")
+done < <(gh repo list $ORG_NAME -L 100 | awk '{print $1}')
 
-# Loop through each repo and delete it if it's not one of the ones to keep
+# Array to hold repos to delete
+repos_to_delete=()
+
+# Loop through each repo and check if it's not one of the ones to keep
 for repo in "${repos[@]}"; do
-    repo_name=$(basename $repo)
-    if [[ ! " ${REPO_ARRAY[@]} " =~ " $repo_name " ]]; then
-        echo "Deleting $repo..."
-        gh repo delete "$repo" --yes
-    else
-        echo "Keeping $repo."
+    repo_name=$(basename "$repo")
+    found=0
+
+    for keep_repo in "${REPO_ARRAY[@]}"; do
+        if [[ "$keep_repo" == "$repo_name" ]]; then
+            found=1
+            break
+        fi
+    done
+
+    if ((found == 0)); then
+        repos_to_delete+=("$repo")
     fi
+done
+
+# Check if there are repos to delete
+if [ ${#repos_to_delete[@]} -eq 0 ]; then
+    echo "No repos found to delete."
+    exit 0
+fi
+
+# Show the repos to be deleted and ask for confirmation
+echo "The following repos will be deleted:"
+for repo in "${repos_to_delete[@]}"; do
+    echo "$repo"
+done
+
+read -p "Are you sure you want to continue? (y/N) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+# Delete the repos
+for repo in "${repos_to_delete[@]}"; do
+    echo "Deleting $repo..."
+    gh repo delete "$repo" --yes
 done
 
 # Remove the "repos" directory
